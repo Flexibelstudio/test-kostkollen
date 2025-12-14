532dcf91980f992a384313a66187d6030f9867af
Karin Birgitta <karin@flexibelfriskvardhalsa.se>
2025-12-14 16:53:31 +0100

    fix

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -1,9 +1,10 @@
+
 import React from 'react';
 
 const SplashScreen: React.FC = () => {
   return (
     <div className="fixed inset-0 bg-neutral-light bg-dotted-pattern bg-dotted-size flex flex-col items-center justify-center z-[100]">
-      <div className="relative animate-pulse">
+      <div className="relative">
         <img
           src="/favicon.png"
           alt="Kostloggen.se logo"
