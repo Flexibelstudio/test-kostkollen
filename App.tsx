fcb8e22997bd9fb15bee1e751a86722d2422d0ba
Karin Birgitta <karin@flexibelfriskvardhalsa.se>
2026-03-08 13:54:23 +0100

    fix

--- App.tsx
+++ App.tsx
@@ -1492,8 +1492,8 @@ if (!uid || userStatus !== 'approved' || !hasCompletedOnboarding) return;
 
   return (
     <>
-      <div className="min-h-screen bg-gray-100 dark:bg-black bg-dotted-pattern bg-dotted-size bg-fixed flex flex-col items-center pb-0">
-       <header className="w-full bg-white dark:bg-neutral-darker text-neutral-dark dark:text-white py-2 px-4 shadow-lg sticky top-0 z-30">
+      <div className="min-h-screen bg-neutral-light bg-dotted-pattern bg-dotted-size bg-fixed flex flex-col items-center pb-0">
+       <header className="w-full bg-white text-neutral-dark py-2 px-4 shadow-lg sticky top-0 z-30">
             <div className="max-w-7xl mx-auto flex items-center justify-between">
                 <div className="flex items-center gap-2 cursor-pointer" onClick={() => setViewMode('main')}>
                     <img src="/favicon.png" alt="Kostloggen.se logo" className="h-14 w-14" />
