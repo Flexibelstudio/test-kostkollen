090d1641174979afe23e88864f3635bfc51af6c5
Karin Birgitta <karin@flexibelfriskvardhalsa.se>
2026-03-26 12:23:35 +0100

    fix

--- index.tsx
+++ index.tsx
@@ -1,6 +1,4 @@
 import './index.css';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App'; // App.tsx now exports 'App' as a named component.
